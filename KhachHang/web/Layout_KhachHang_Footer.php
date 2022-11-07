86cc64feb9893c776b6d5ec0b1a9cb6adef28a9f
Trinh Minh Hau <hau.tm.61cntt@ntu.edu.vn>
2022-11-07 22:37:05 +0700

    Khach Hang Lan 1

---
+++ KhachHang/web/Layout_KhachHang_Footer.php
@@ -0,0 +1,4 @@
+<div style="clear: both ;"></div>
+<div class="footer-left1" style="background-color: black;">
+    <p style=" width:100%; height:30px; text-align:center; color:white">Copyright by Nhóm 11</p>
+</div>
