e0faa1ae3008f9e2a3babefd9e673732c662893d
Trinh Minh Hau <hau.tm.61cntt@ntu.edu.vn>
2022-11-29 16:31:38 +0700

    Sua VeChungtoi, fix timkiem phan trang

--- KhachHang/web/VeChungToi.php
+++ KhachHang/web/VeChungToi.php
@@ -8,93 +8,49 @@
 <?php include("Layout_KhachHang_Header.php"); ?>
 
 <head>
-    <title>Xem ví dụ</title>
-    <style type="text/css">
-        table,
-        th,
-        td {
-            border: 1px solid #868585;
 
-        }
-
-        table {
-            border-collapse: collapse;
-
-            width: 80%;
-            margin: 0 auto;
-
-        }
-
-        th,
-        td {
-            text-align: center;
-            padding: 10px;
-        }
-
-        table tr:nth-child(odd) {
-            background-color: #eee;
-        }
-
-        table tr:nth-child(even) {
-            background-color: white;
-        }
-
-        table tr:nth-child(1) {
-            background-color: skyblue;
-        }
-    </style>
 </head>
 
 <body>
-    <br>
-    <h1 style="color:black;text-align:center">THÔNG TIN</h1>
-    <BR>
-    <table>
-        <tr>
-            <th>STT</th>
-            <th>Họ và tên</th>
-            <th>Mã số sinh viên</th>
-            <th>Giới tính</th>
-            <th>Quê quán</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>Trịnh Minh Hậu</td>
-            <td>61133622</td>
-            <td>Nam</td>
-            <td>Ninh Hòa</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Lê Như Của</td>
-            <td>61133452</td>
-            <td>Nam</td>
-            <td>Ninh Hòa</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>Nguyễn Ngọc Hồng Hân</td>
-            <td>61132924</td>
-            <td>Nữ</td>
-            <td>Nha Trang</td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <td>Dương Quốc Phong</td>
-            <td>61130851</td>
-            <td>Nam</td>
-            <td>Diên Khánh</td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <td>Hồ Anh Nam</td>
-            <td>611133984</td>
-            <td>Nam</td>
-            <td>Ninh Hòa</td>
-        </tr>
-    </table>
-    <br>
+
+    <div class="Menu" style="text-align:center">
+        <a <?php if (isset($_GET['thamso']) and $_GET['thamso'] == './Member/MinhHau.php') echo "class='active'" ?>href="?thamso=./Member/MinhHau.php">Trịnh Minh Hậu</a>
+        <a <?php if (isset($_GET['thamso']) and $_GET['thamso'] == './Member/NhuCua.php') echo "class='active'" ?> href="?thamso=./Member/NhuCua.php">Lê Như Của</a>
+        <a <?php if (isset($_GET['thamso']) and $_GET['thamso'] == './Member/AnhNam.php') echo "class='active'" ?>href="?thamso=./Member/AnhNam.php">Hồ Anh Nam</a>
+        <a <?php if (isset($_GET['thamso']) and $_GET['thamso'] == './Member/HongHan.php') echo "class='active'" ?>href="?thamso=./Member/HongHan.php">Nguyễn Ngọc Hồng Hân</a>
+        <a <?php if (isset($_GET['thamso']) and $_GET['thamso'] == './Member/QuocPhong.php') echo "class='active'" ?>href="?thamso=./Member/QuocPhong.php">Dương Quốc Phong</a>
+    </div>
+    <div class="noidung">
+        <?php if (isset($_GET['thamso'])) {
+            include($_GET['thamso']);
+        } ?>
+    </div>
 </body>
+<style>
+    .Menu {
+        margin-top: 24px;
+    }
+
+    .Menu a {
+
+        margin-left: 10px;
+        padding: 10px 15px;
+        background: #fff;
+        color: #333;
+        transition: 1s;
+    }
+
+    .Menu a.active {
+        background-color: #333;
+        color: #fff
+    }
+
+    .Menu a:hover {
+
+        background: #333;
+        color: #fff
+    }
+</style>
 <?php include("Layout_KhachHang_Footer.php"); ?>
 
 </html>
